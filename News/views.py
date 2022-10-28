b168c9a7f0e466b01cdd3fb319e7911be305b86d
Pobezhal <104855548+Pobezhal@users.noreply.github.com>
2022-10-28 20:51:34 +0300

    Add files via upload

---
+++ News/views.py
@@ -0,0 +1,80 @@
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import *
+from datetime import datetime
+from .filters import *
+from .forms import *
+
+
+class PostList(ListView):
+
+    model = Post
+    ordering = '-post_time'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['filterset'] = self.filterset
+        return context
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'postdetail.html'
+    context_object_name = 'post'
+
+
+def create_post(request):
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/news/')
+
+    return render(request, 'post_edit.html', {'form': form})
+
+
+class PostCreate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
+
+
+
+class PostSearcher(ListView):
+    model = Post
+    template_name = 'post_search.html'
+    context_object_name = 'post_search'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
